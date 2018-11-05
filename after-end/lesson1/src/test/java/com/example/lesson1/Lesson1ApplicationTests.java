116a4317719be4ef7b3fbb37b4d9c4f4ad79dcf8
codeyoyo <codeyoyo@163.com>
2018-11-05 22:11:04 +0800

    lesson1上传

---
+++ after-end/lesson1/src/test/java/com/example/lesson1/Lesson1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.lesson1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Lesson1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
