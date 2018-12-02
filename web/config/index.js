c186aeb20990cafe1aba1140d0fc39b9047f1a6b
codeyoyo <codeyoyo@163.com>
2018-12-02 22:58:00 +0800

    Spring lesson

--- web/config/index.js
+++ web/config/index.js
@@ -38,12 +38,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../Spoot-Lesson/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../../Spoot-Lesson'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
